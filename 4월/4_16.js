6f3f2cb3356d87105846d066a3b4cc0b9f3ddb4c
JinS98 <fnvl2358@naver.com>
2024-04-17 18:40:56 +0900

    4/17

--- 4월/4_16.js
+++ 4월/4_16.js
@@ -66,3 +66,29 @@ function solution(code) {
 }
 
 solution("abc11acb111ac")
+
+// 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+
+function solution(strings, n) {
+    var answer = [];
+    let arr = []
+    let strs = strings.sort()
+    let a = 0
+    for(let i = 0 ; i < strs.length;i++) {
+      answer.push(strings[i][n])
+    }
+    answer.sort()
+    for(let i = 0; i < answer.length;i++) {
+      for(let j = 0; j < strs.length;j++) {
+        if(answer[i] === strs[j][n]) {
+            arr.push(strs[j]) 
+        }
+      }
+    }
+  let result = new Set(arr)
+  result = [...result]
+    return result;
+}
+
+solution(["sun", "bed", "car"],	1)
+solution(["bbce", "bbcd","bbcf", "acad", "cdx"],	2)
